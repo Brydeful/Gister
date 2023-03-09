62efa526733efd74f0036823ed34074c8d3667d5
Vyacheslav Grudkin <slava8055@gmail.com>
2023-03-09 16:29:25 +0500

    Initial Commit

---
+++ Gister/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gister
+//
+//  Created by Вячеслав Грудкин on 09.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
